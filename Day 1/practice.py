33756e8f60100a9275734eb7c99f3b18e6f63060
Henri Pickford <henripickford@gmail.com>
2026-05-27 20:25:04 -0400

    Started writing notes and testing stuff out

---
+++ Day 1/practice.py
@@ -0,0 +1,70 @@
+#test single line comment
+"""
+test multi line comment can
+do multiple lines
+"""
+
+#Data types
+
+"""
+Numbers
+Integer: Integer(negative, zero and positive) numbers Example: ... -3, -2, -1, 0, 1, 2, 3 ...
+Float: Decimal number Example ... -3.5, -2.25, -1.0, 0.0, 1.1, 2.2, 3.5 ...
+Complex Example 1 + j, 2 + 4j
+"""
+
+x= 10 #integer
+y= 3.14 #float
+z= 1 + 2j #complex
+
+print(x)
+print(y)
+print(z)
+
+#just testing the print function and how it works with different data types
+
+"""
+String: A collection of one or more characters under a single or double quote. If a string is more than one sentence then we use a triple quote.
+examples: 'Hello World', "Python is great", '''This is a multi line string
+and it can span multiple lines.'''
+"""
+
+name = 'hello world'
+multiple_line_string = '''multi line string example
+this is the second line'''
+
+print(name)
+print(multiple_line_string)
+
+"""
+Boolean: A data type that can only have two values: True or False.
+"""
+is_true = True
+is_false = False
+
+print(is_true)
+print(is_false)
+
+"""
+List: A collection of different data types that are ordered and changeable. Lists are written with square brackets [].
+Example: [1, 2, 3, 'hello', True]
+"""
+
+Fruits = ["apple", "banana", "orange", "grape"]
+profile_info = ["John morgan", 30, "electrician", True]
+
+print(Fruits)
+print(profile_info)
+
+"""
+dictionary: A collection of key-value (like a label or identifier of a value) pairs that are unordered, changeable and indexed. 
+Dictionaries are written with curly brackets {}.
+Example: {'name': 'John', 'age': 30, 'city': 'New York'}
+multi line dictionary example:
+my_dict = {
+    'name': 'John',
+    'age': 30,
+    'city': 'New York'
+}
+"""
+
