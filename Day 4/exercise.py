67069efd302c045371de73efcfec87de26cdf783
Henri Pickford <henripickford@gmail.com>
2026-06-02 21:42:07 -0400

    Started day 4 exercises and Applied for github student bundle

---
+++ Day 4/exercise.py
@@ -0,0 +1,29 @@
+course = ' '.join(['Thirty', 'Days', 'Of', 'Python'])
+print(course)
+
+goal = ' '.join(['Coding', 'For', 'All'])
+print(goal)
+
+company = 'Coding For All'
+
+print(company)
+
+print(len(company))
+
+company_upper = company.upper()
+
+company_lower = company.lower()
+
+capitalized_company = company.capitalize()
+
+title_company = company.title()
+
+swapcase_company = company.swapcase()
+
+coding_excluded = company[7:]
+print(coding_excluded)
+
+print(company.find('Coding'))
+
+coding_replaced = company.replace('Coding', 'Python')
+print(coding_replaced)
