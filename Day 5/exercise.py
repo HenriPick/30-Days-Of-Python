111944aa3d8104b25c09d074b09a4842e7eb0018
Henri Pickford <henripickford@gmail.com>
2026-06-11 22:06:38 -0400

    Finished day 5 and updated README

--- Day 5/exercise.py
+++ Day 5/exercise.py
@@ -72,7 +72,244 @@ ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 ages.sort()
 print(ages)
 min_age = ages[0]
-print(f'max age:', min)
+print(f'max age:', min_age)
 max_age = ages[-1]
-print(f'min age', max)
+print(f'min age:', max_age)
+ages.insert(0, min_age)
+ages.insert(-1, max_age)
 
+median_age = ages[len(ages) // 2] 
+if len(ages) % 2 == 0:
+    median_age = (ages[len(ages) // 2 - 1] + ages[len(ages) // 2]) / 2
+print(median_age)
+
+total_age = 0
+for i in ages:
+    total_age += i
+
+average_age = total_age // len(ages)
+print(average_age)
+
+range_of_ages = max_age - min_age
+print(range_of_ages)
+
+print(abs(min_age - average_age) >= abs(max_age - average_age))
+
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cabo Verde',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo, Democratic Republic of the',
+  'Congo, Republic of the',
+  'Costa Rica',
+  "Côte d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor-Leste)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Eswatini',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'North Macedonia',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Palestine',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent and the Grenadines',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+]
+
+middle_country = len(countries) // 2
+if len(countries) % 2 == 0:
+    print(countries[middle_country - 1] + ' and ' + countries[middle_country])
+else:
+    print(countries[middle_country])
+    
+if len(countries) % 2 == 0:
+    list_1 = countries[0: middle_country]
+    list_2 = countries[middle_country: ]
+else:
+    list_1 = countries[0: middle_country + 1]
+    list_2 = countries[middle_country + 1: ]
+print("List 1:", list_1)
+print("List 2:", list_2)
+
+country_1, country_2, country_3, *scandic_countries = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+print(country_1)
+print(country_2)
+print(country_3)
+print(scandic_countries)
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Welcome to my independent study project! This repository tracks my progress thro
 | 2 | Variables and Built in functions | Practiced basic exercises with variables and built in python functions. | [Day 2 Code](./Day%202/exercise.py)
 | 3 | Operators | Learned and Practiced Basic operators with exercises (Note: Accidentally labeled "Day 2" in commit) | [Day 3 Code](./Day%203/exercise.py)
 | 4 | String & Data Types Basics | Learned about string manipulation, indexing, and basic data types while practicing with foundational exercises. | [Day 4 Code](./Day%204/exercise.py) |
+| 5 | Lists | Learned about lists, their properties and built in functions | [Day 5 Code](./Day%205/exercise.py) |
 ---
 
 Small note : Practice files are basically just experimenting and taking notes and Exercise files are exercises from "30-Days-Of-Python"
