a05301087164c7310ab96847f100fb6a1982c890
Henri Pickford <henripickford@gmail.com>
2026-08-19 21:56:09 -0400

    continued day 11 exercises

--- Day 11/exercise.py
+++ Day 11/exercise.py
@@ -13,7 +13,7 @@ print(area_of_circle(3))
 def add_all_nums(*nums):
     total = 0
     for num in nums:
-        if isinstance(num, int):
+        if isinstance(num, (int, float)):
             total += num
         else:
             return('Please input valid numbers')
@@ -28,3 +28,106 @@ def convert_celsius_to_fahrenheit(celsius):
 print(convert_celsius_to_fahrenheit(0))
 
 
+def check_season(month):
+        if month in ['December', 'January', 'February']:
+            return('Winter')
+        elif month in ['March', 'April', 'May']:
+            return('Spring')
+        elif month in ['June', 'July', 'August']:
+            return('Summer')
+        elif month in ['September', 'October', 'November']:
+            return('Fall')
+        else:
+            return('Input a valid month with a capital letter at the start')
+    
+month = 'January'  
+print(f'The month {month} is in {check_season(month)}')
+
+def solve_quadratic_eqn(a, b, c):
+    discriminant = (b**2) - (4*a*c)
+    root1 = (-b - discriminant**0.5) / (2*a)
+    root2 = (-b + discriminant**0.5) / (2*a)
+    return root1, root2
+
+print(solve_quadratic_eqn(2, 5, 3))
+
+def print_list(a):
+    if isinstance(a, list):
+        for i in a:
+            print(i)
+    else:
+        print('Please enter a list as an argument for this function')
+        
+print_list([1, 2, 3, 4, 5, 6])
+print_list('hello')
+
+def reverse_list(a):
+    if isinstance(a, list):
+        index = -1
+        for i in a:
+            print(a[index])
+            index -= 1
+    else:
+        print('Please enter a list as an argument for this function')
+        
+reverse_list([1, 2, 3, 4, 5])
+        
+def capitalize_list_items(a):
+    list_copy = a
+    if isinstance(list_copy, list):
+        index = 0
+        for i in list_copy:
+            list_copy[index] = i.capitalize()
+            index += 1
+        return list_copy
+    else:
+        print('Please enter a list as an argument for this function')
+        
+print(capitalize_list_items(['Hello', 'gg', 'llllllll', 'qwerty']))
+
+def add_item(a, item):
+    a.append(item)
+    
+list_1 = [1, 2, 3, 4]
+add_item(list_1, 5)
+print(list_1)
+
+
+def remove_item(a, item):
+    list_copy = a.copy()
+    if isinstance(list_copy, list):
+        while item in list_copy:
+            list_copy.remove(item)
+        return list_copy
+    else:
+        print('Please enter a list as an argument for this function')
+
+food_stuff = ['Potato', 'Tomato', 'Mango', 'Milk', 'Mango'] 
+print(remove_item(food_stuff, 'Mango'))
+
+
+def sum_of_numbers(a):
+    total = 0
+    for i in range(a+1):
+        total += i
+    return total
+
+print(sum_of_numbers(5))
+
+def sum_of_odds(a):
+    total = 0
+    for i in range(a+1):
+        if i % 2 != 0:
+            total += i
+    return total
+
+print(sum_of_odds(5))
+
+def sum_of_even(a):
+    total = 0
+    for i in range(a+1):
+        if i % 2 == 0:
+            total += i
+    return total
+
+print(sum_of_even(5))
